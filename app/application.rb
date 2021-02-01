fa6e64f45cec399442f053f49f26d655b9b4b887
Jacob Hampton <jhampton792@gmail.com>
2021-02-01 10:10:40 -0600

    commit tree 0661e09dd42d11095cfbe3d317ca4b9aa8f1e554

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,19 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    num1 = Kernel.rand(1..20)
+    num2 = Kernel.rand(1..20)
+    num3 = Kernel.rand(1..20)
+
+    if num1 == num2 && num2 == num3
+      resp.write "You Win!\n"
+    else
+      resp.write "You Lose!\n"
+    end
+
+    resp.write "#{num1}, #{num2}, #{num3}"
+    # resp.write "Hello, World"
     resp.finish
   end
 
